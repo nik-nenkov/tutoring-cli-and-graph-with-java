04361b29596cb1482284d8b041f5355fdeb1d0b1
Nik Nenkov <n.nenkov@yahoo.com>
2018-08-17 14:05:57 +0300

    now have table, printing and main menu, next is the parser

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,45 @@
+import expression.ExpressionParser;
 import table.Table;
 
+import java.util.Scanner;
+
 public class Application {
-    private static Table t = new Table(2,2);
+    private static final Table myTable = new Table();
+    private static final Scanner scan = new Scanner(System.in);
+    private static final String APPLICATION_INFORMATION = "2018 <epam> IS AWESOME!";
+    private static final String WELCOME_MESSAGE = "Try /info, /print, /exit or an expression";
+    private static final String GOODBYE_MESSAGE = "See ya later User!";
+    private static boolean isRunning = true;
+    private static String lastInput;
+    
+
     public static void main(String[] args) {
-        System.out.println(t.toPrint());
+        print(WELCOME_MESSAGE);
+        while(isRunning){
+            lastInput = scan.nextLine();
+            processQuery();
+        }
+        print(GOODBYE_MESSAGE);
+    }
+
+    private static void processQuery() {
+        switch(lastInput){
+            case("/print"):
+                print(myTable.toPrint());
+                break;
+            case("/exit"):
+                isRunning = false;
+                break;
+            case("/info"):
+                print(APPLICATION_INFORMATION);
+                break;
+            default:
+                ExpressionParser.parse(lastInput, myTable);
+                break;
+        }
+    }
+
+    private static void print(String message){
+        System.out.println(message);
     }
 }
---
+++ src/main/java/expression/ExpressionParser.java
@@ -0,0 +1,9 @@
+package expression;
+
+import table.Table;
+
+public class ExpressionParser {
+    public static void parse(String lastInput, Table myTable) {
+
+    }
+}
